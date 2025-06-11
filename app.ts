23f3bf97a778b93da76c0f6b2f4a55f2ec048d14
Simon Maarawi <maarawisimon8@gmail.com>
2025-06-11 15:28:25 +0300

    feat: add a note

--- app.ts
+++ app.ts
@@ -1,2 +1,27 @@
 import yargs from 'yargs';
-import { hidebin } from 'yargs/helpers';
+import { hideBin } from 'yargs/helpers';
+import { addNote } from './notes';
+
+yargs(hideBin(process.argv))
+  .command({
+    command: 'add',
+    describe: 'Add a new note',
+    builder: {
+        title: {
+            describe: 'Note title',
+            demandOption: true,
+            type: 'string',
+        },
+        body: {
+            describe: 'Note body',
+            demandOption: true,
+            type: 'string',
+        }
+    },
+    handler(argv) {
+        if (typeof argv.title === 'string' && typeof argv.body === 'string') {
+            addNote(argv.title, argv.body);
+        }
+    }
+  })
+  .parse();
