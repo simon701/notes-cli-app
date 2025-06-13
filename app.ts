c74e88663e46481ff6d1fb010478bba7a2471811
Simon Maarawi <68278008+simon701@users.noreply.github.com>
2025-06-13 19:03:56 +0300

    feat: implement full CLI note management (add, list, read, remove) (#2)
    * feat: add a note
    
    * feat: add a note
    
    * feat: list all notes
    
    * feat: read a note by title
    
    * feat: remove a note
    
    * feat: implement add, list, read, and remove commands for Notes CLI
    
    * fix: temporary changes before rebase
    
    * fix: ensure formatting is correct before merging into main

--- app.ts
+++ app.ts
@@ -1,2 +1,66 @@
-import yargs from 'yargs';
-import { hidebin } from 'yargs/helpers';
+import yargs from "yargs";
+import { hideBin } from "yargs/helpers";
+import { addNote, listNotes, readByTitle, removeFromList } from "./notes";
+
+yargs(hideBin(process.argv))
+  .command({
+    command: "add",
+    describe: "Add a new note",
+    builder: {
+      title: {
+        describe: "Note title",
+        demandOption: true,
+        type: "string",
+      },
+      body: {
+        describe: "Note body",
+        demandOption: true,
+        type: "string",
+      },
+    },
+    handler(argv) {
+      if (typeof argv.title === "string" && typeof argv.body === "string") {
+        addNote(argv.title, argv.body);
+      }
+    },
+  })
+  .command({
+    command: "list",
+    describe: "List all notes",
+    handler() {
+      listNotes();
+    },
+  })
+  .command({
+    command: "read",
+    describe: "Read a note by Title",
+    builder: {
+      title: {
+        describe: "Note title",
+        demandOption: true,
+        type: "string",
+      },
+    },
+    handler(argv) {
+      if (typeof argv.title === "string") {
+        readByTitle(argv.title);
+      }
+    },
+  })
+  .command({
+    command: "remove",
+    describe: "Remove note from list",
+    builder: {
+      title: {
+        describe: "Note title",
+        demandOption: true,
+        type: "string",
+      },
+    },
+    handler(argv) {
+      if (typeof argv.title === "string") {
+        removeFromList(argv.title);
+      }
+    },
+  })
+  .parse();
